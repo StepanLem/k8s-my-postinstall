90570eae4c648a68643d8dca5dc705db4a378fae
Stepan Lemtyugin <stepanlemtugin@gmail.com>
2025-11-09 06:36:28 +0500

    update postgres passwords paths

--- envs/dev2/apps/0.kube-infra/eso/resources/vault-token.bash
+++ envs/dev2/apps/0.kube-infra/eso/resources/vault-token.bash
@@ -8,3 +8,8 @@ type: Opaque
 stringData:
   token: <your-token> 
 EOF
+
+
+# В папке resources:
+- Лежит ClusterSecretStore. Он описывает как подключаться к vault. но ему нужно создать токен-секрет.
+-(НЕ СМЕЙ ЗАПУШИТЬ В git) Секрет создаётся вручную: код в resources/vault-token.bash - скопируй код в терминал, вставь токен из vault и выполнить.
